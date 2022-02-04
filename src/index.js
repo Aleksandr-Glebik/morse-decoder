b7320568d8461d80fa13c6419dde5e011c726281
Aleksandr-Glebik <glebikaa@mail.ru>
2022-02-04 22:58:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let newArr = expr.split('')
+    // console.log('newArr', newArr);
+    let newArrForSymbol = []
+    for (let i = 0; i < newArr.length / 10; i++) {
+        newArrForSymbol.push(newArr.slice(i * 10, (i + 1) * 10))
+    }
+    // console.log('newArrForSymbol', newArrForSymbol);
+    let arrTenOrEleven = []
+    newArrForSymbol.forEach((elem) => {
+        let ind = elem.indexOf('1')
+        arrTenOrEleven.push(elem.slice(ind))
+    })
+    // console.log("arrTenOrEleven", arrTenOrEleven);
+
+    let itemArrSymbol = []
+    arrTenOrEleven.forEach((item) => {
+        let arrSymbol = []
+        for (let i = 0; i < item.length;i++) {
+            if (item[i] + item[i + 1] === '10') {
+                arrSymbol.push('.')
+            } else if (item[i] + item[i + 1] === '11' && i % 2 === 0) {
+                arrSymbol.push('-')
+            } else if (item[i] === '*') {
+                arrSymbol.push(' ')
+            }
+        }
+        // console.log('arrSymbol last', arrSymbol);
+        itemArrSymbol.push(arrSymbol)
+    })
+    // console.log('itemArrSymbol', itemArrSymbol);
+    let result = []
+    itemArrSymbol.forEach((elem) => {
+        let sumRow = ''
+        for (let i = 0; i < elem.length; i++) {
+            sumRow += elem[i]
+        }
+        // console.log('sumRow', sumRow);
+        if (sumRow === ' ') {
+            result.push(' ')
+        } else {
+            result.push(MORSE_TABLE[sumRow])
+        }
+        // console.log('result', result);
+    })
+    // console.log('result cancel', result);
+    let sumStr = ''
+    result.forEach((el) => {
+        sumStr += el
+    })
+    console.log('sumStr', sumStr);
+    return sumStr
 }
 
 module.exports = {
